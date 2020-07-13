9a2f6acd478a990498aac3612637e222ddd0f7ea
idsherma <idsherma@gmail.com>
2020-07-13 17:09:43 -0400

    init commit

---
+++ components/DirectoryComponent.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { View, FlatList, Text } from 'react-native';
+import { ListItem } from 'react-native-elements';
+import { Tile } from 'react-native-elements';
+import { connect } from 'react-redux';
+import Loading from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+        campsites: state.campsites
+    };
+};
+
+
+class Directory extends Component {
+
+    static navigationOptions = {
+        title: 'Directory'
+    };
+
+    render() {
+        const { navigate } = this.props.navigation;
+
+        const renderDirectoryItem = ({item}) => {
+            return (
+                <Tile
+                    title={item.name}
+                    caption={item.description}
+                    featured
+                    onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
+                    imageSrc={{uri: baseUrl + item.image}}
+                />
+            );
+        };
+
+        if (this.props.campsites.isLoading) {
+            return <Loading />;
+        }
+        
+        if (this.props.campsites.errMess) {
+            return (
+                <View>
+                    <Text>{this.props.campsites.errMess}</Text>
+               </View>
+            );
+        }
+
+        return (
+            <FlatList
+                data={this.props.campsites.campsites}
+                renderItem={renderDirectoryItem}
+                keyExtractor={item => item.id.toString()}
+            />
+        );
+    }
+}
+
+export default connect(mapStateToProps)(Directory);
---
+++ shared/baseUrl.js
@@ -0,0 +1,3 @@
+//export const baseUrl = 'http://192.168.1.153:3001/';
+
+export const baseUrl = 'http://10.0.2.2:3001/';
