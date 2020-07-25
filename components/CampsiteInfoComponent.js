75e75fbf8dffd5fe1bd29ff3e6d629e7970165bb
idsherma <idsherma@gmail.com>
2020-07-25 12:58:29 -0400

    final saturday workshop

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -30,7 +30,7 @@ function RenderCampsite(props) {
 
     const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
 
-    const recognizeComment = ({dx}) => (dx > -200) ? true : false;
+    const recognizeComment = ({dx}) => (dx > 200) ? true : false;
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
@@ -41,27 +41,28 @@ function RenderCampsite(props) {
         onPanResponderEnd: (e, gestureState) => {
             console.log('pan responder end', gestureState);
             if (recognizeDrag(gestureState)) {
-                Alert.alert(
-                    'Add Favorite',
-                    'Are you sure you wish to add ' + campsite.name + ' to favorites?',
-                    [
-                        {
-                            text: 'Cancel',
-                            style: 'cancel',
-                            onPress: () => console.log('Cancel Pressed')
-                        },
-                        {
-                            text: 'OK',
-                            onPress: () => props.favorite ?
-                                console.log('Already set as a favorite') : props.markFavorite()
-                        }
-                    ],
-                    { cancelable: false }
-                );
-            } else if (recognizeDrag(gestureState) === false) {
-                recognizeComment(gestureState);
+              Alert.alert(
+                'Add Favorite',
+                'Are you sure you wish to add ' + campsite.name + ' to favorites?',
+                [
+                  {
+                    text: 'Cancel',
+                    style: 'cancel',
+                    onPress: () => console.log('Cancel Pressed')
+                  },
+                  {
+                    text: 'OK',
+                    onPress: () => props.favorite ? console.log('Already set as a favorite') : props.markFavorite()
+                  }
+                ],
+                { cancelable: false }
+              );
+            } else if (recognizeComment(gestureState)) {
                 props.onShowModal();
             }
+      
+            return true;
+
         }
     });
 
--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -6,6 +6,9 @@ import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 import { createBottomTabNavigator } from 'react-navigation';
 import { baseUrl } from '../shared/baseUrl';
+import * as ImageManipulator from "expo-image-manipulator";
+import * as MediaLibrary from 'expo-media-library';
+
 
 class LoginTab extends Component {
 
@@ -148,13 +151,48 @@ class RegisterTab extends Component {
         const cameraRollPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
 
         if (cameraPermission.status === 'granted' && cameraRollPermission.status === 'granted') {
+            
             const capturedImage = await ImagePicker.launchCameraAsync({
                 allowsEditing: true,
                 aspect: [1, 1]
-            });
+            })
+
             if (!capturedImage.cancelled) {
                 console.log(capturedImage);
-                this.setState({imageUrl: capturedImage.uri});
+                MediaLibrary.createAssetAsync(capturedImage.uri);
+                this.processImage(capturedImage.uri);
+            }
+            
+        }
+    }
+
+    processImage = async (imgUri) => {
+        const processedImage = await ImageManipulator.manipulateAsync(
+            imgUri,
+            [
+                { resize: {width: 400} }
+            ], 
+            { format: 'png' }
+        );
+
+        console.log(processedImage);
+
+        this.setState({ imageUrl: processedImage.uri }) 
+
+    }
+
+    getImageFromGallery = async () => {
+        const cameraRollPermissions = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+        if (cameraRollPermissions.status === 'granted') {
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [1, 1]
+            });
+
+            if (!capturedImage.cancelled) {
+                console.log(capturedImage);
+                this.processImage(capturedImage.uri);
             }
         }
     }
@@ -184,6 +222,10 @@ class RegisterTab extends Component {
                         <Button
                             title='Camera'
                             onPress={this.getImageFromCamera}
+                        />
+                        <Button
+                            title='Gallery'
+                            onPress={this.getImageFromGallery}
                         />
                     </View>
                     <Input
